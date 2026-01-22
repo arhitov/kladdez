0687208a48eae8f658746c3dbb63c6740e2b02d8
Alexander Arhitov <clgsru@gmail.com>
2026-01-22 22:34:15 +0300

    Request Limit

--- golang/http/request/limit.go
+++ golang/http/request/limit.go
@@ -1,5 +1,6 @@
 package request
 
 type Limit struct {
-	Limit int `json:"limit,omitempty" key:"limit" validate:"omitempty,numeric,min=0,max=1000"`
+	Limit  int `json:"limit,omitempty"  key:"limit"  validate:"omitempty,numeric,min=0,max=1000"`
+	Offset int `json:"offset,omitempty" key:"offset" validate:"omitempty,numeric,min=0"`
 }
