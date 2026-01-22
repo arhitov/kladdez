98e62af733c8a3aa39f6b3d34a788936ac05de1f
Alexander Arhitov <clgsru@gmail.com>
2026-01-22 22:36:11 +0300

    Request Limit => LimitOffset

--- golang/http/request/limit.go
+++ golang/http/request/limit.go
@@ -1,6 +1,6 @@
 package request
 
-type Limit struct {
+type LimitOffset struct {
 	Limit  int `json:"limit,omitempty"  key:"limit"  validate:"omitempty,numeric,min=0,max=1000"`
 	Offset int `json:"offset,omitempty" key:"offset" validate:"omitempty,numeric,min=0"`
 }
